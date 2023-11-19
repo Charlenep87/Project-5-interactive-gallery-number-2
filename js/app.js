70da40b5a722e465bdddf24a4b9b96498e403328
Charlene Pezarkar <charlene.pezarkar@gmail.com>
2023-11-19 10:44:59 -0500

    Centered images and modify for the description to appear under the pictures

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $("#search").on("keyup", function() {
     const value = $(this)
         .val().toLowerCase();
     $(".grid a").filter(function() {
-        $(this).toggle($(this).attr("data-title").toLowerCase().indexOf(value) > -1
+        $(this).toggle($(this).attr("data-caption").toLowerCase().indexOf(value) > -1
         );
     });
 });
